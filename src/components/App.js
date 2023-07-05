8cad5970343001c7677bc9eb11d766ea6df4e7f3
sabir <sksabiruddin81@gmail.com>
2023-07-05 08:36:40 +0530

    submitting code-Wed Jul 05 2023 08:36:36 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,26 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
+import "./../styles/App.css";
 
+const App = () => {
+  const arr = ["Tab 1", "Tab 2", "Tab 3"];
+  const [click, setClick] = useState("");
+
+  const handleTabClick = (details) => {
+    setClick(details);
+  };
+
+  return (
+    <div>
+      <ul>
+        {arr.map((details, index) => (
+          <li key={index} onClick={() => handleTabClick(details)}>
+            {details}
+          </li>
+        ))}
+      </ul>
+      <p>This is the content for {click}</p>
+    </div>
+  );
+};
+
+export default App;
